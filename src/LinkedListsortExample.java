021a7b5d7dc47a7f99dd99519665d9fd0b46134f
Touyer <touyer.lees@gmail.com>
2024-12-11 00:08:36 -0600

    GLAB 303.11.4 LinkedList Process completed

--- src/LinkedListsortExample.java
+++ src/LinkedListsortExample.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Collections;
+import java.util.LinkedList;
 
 public class LinkedListsortExample {
+    public static void main(String[] args) {
+        //Create linked list
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("A");
+        linkedList.add("C");
+        linkedList.add("B");
+        linkedList.add("D");
+
+        //Unsorted
+        System.out.println(linkedList);
+
+        //1. Sort the list
+        Collections.sort(linkedList);
+        //Sorted
+        System.out.println(linkedList); //Output A, B, C, D
+
+        //2. Custom sorting
+        Collections.sort(linkedList, Collections.reverseOrder());
+        //Custom sorted
+        System.out.println(linkedList); // Output D, C, B, A
+
+
+    }
+
 }
